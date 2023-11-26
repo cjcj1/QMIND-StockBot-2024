f15552cf73f6ddade323d460471a8d56f961ea87
cjcj1 <johnston5.connor@gmail.com>
2023-11-25 23:34:55 -0500

    added dividend dataframe

--- data_exploration.py
+++ data_exploration.py
@@ -1,25 +1,15 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-import numpy as np
 import matplotlib.pyplot as plt
 
 # reading data from csv
-adj = pd.read_csv('Stocks_AdjClose_3yr.csv')
-vol = pd.read_csv('Stocks_Volume_3yr.csv')
+adj = pd.read_csv('Stocks_AdjClose_3yr.csv', index_col=0, parse_dates=True)
+vol = pd.read_csv('Stocks_Volume_3yr.csv', index_col=0, parse_dates=True)
 
-# changing index to datetime
-timestamps = pd.to_datetime(adj.index).astype('int64').astype(int) / 10**9
-
-# calculating linear regression
-lin_reg = LinearRegression().fit(timestamps.values.reshape(-1, 1), adj["AAPL"])
-coefficients = np.polyfit(timestamps, adj["AAPL"], 1)
-
-# plotting linear regression
+# plotting data
 plt.scatter(adj.index, adj["AAPL"], label='Data Points')
-plt.plot(adj.index, coefficients[0] * timestamps + coefficients[1], color='red', label='Linear Regression')
-plt.xlabel('')
-plt.xticks([])
+plt.xticks(rotation=45)
+plt.xlabel('Date')
 plt.ylabel('Adjusted Close')
-plt.legend().set_visible(False)
 plt.title('AAPL')
+plt.subplots_adjust(bottom=0.2)
 plt.show()
--- data_setup.py
+++ data_setup.py
@@ -2,22 +2,55 @@ import pandas as pd
 import yfinance as yf
 import requests
 from bs4 import BeautifulSoup
+from io import StringIO
 
 response = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 html_content = response.content
 soup = BeautifulSoup(html_content, 'html.parser')
 tickers = soup.find_all('table')[0]
-stocks = pd.read_html(str(tickers))[0]
+stocks = pd.read_html(StringIO(str(tickers)))[0]
 stocks = stocks.dropna(axis=1)
 
 df = yf.download(stocks.Symbol.to_list(), start="2020-11-01", end="2023-10-30")
 df = df.dropna(axis=1)
-df_open = df["Open"]
-df_high = df["High"]
-df_low = df["Low"]
-df_close = df["Close"]
-df_adj_close = df["Adj Close"]
-df_vol = df["Volume"]
-print(df_open.to_string())
-df_adj_close.to_csv('Stocks_AdjClose_3yr.csv')
-df_vol.to_csv('Stocks_Volume_3yr.csv')
+df.index = pd.to_datetime(df.index.get_level_values(0))
+df.index = df.index.date
+adj = df["Adj Close"]
+vol = df["Volume"]
+# adj.to_csv('Stocks_AdjClose_3yr.csv')
+# vol.to_csv('Stocks_Volume_3yr.csv')
+
+divs_list = []
+divs = pd.DataFrame()
+
+for ticker_symbol in stocks.Symbol:
+    stock_divs = yf.Ticker(ticker_symbol).dividends
+    stock_divs = pd.Series(stock_divs)
+    stock_divs = stock_divs.loc["2020-11-01":"2023-10-30"]
+    if len(stock_divs) > 0:
+        divs_list.append(stock_divs.rename(ticker_symbol))
+
+divs = pd.concat(divs_list, axis=1)
+
+divs = divs.dropna(how='all')
+new_divs = pd.DataFrame()
+new_divs["placeholder"] = adj["A"]
+new_divs["placeholder"].values[:] = 0
+divs = pd.concat([new_divs, divs])
+divs = divs.dropna(how='all')
+
+divs = divs.drop(columns=["placeholder"])
+divs = divs.reindex(sorted(divs.columns), axis=1)
+
+
+def update_early_cells(column):
+    first_non_na = column.first_valid_index()
+    if first_non_na is not None:
+        divs.loc[:first_non_na, column.name] = column[first_non_na]
+
+
+divs.apply(update_early_cells, axis=0)
+
+divs = divs.fillna(method='ffill')
+
+# divs.to_csv('Stocks_Dividends_3yr.csv')
