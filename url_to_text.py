456455ce5e842a1f1b1bc69eb17dcbb13c5606c4
cjcj1 <johnston5.connor@gmail.com>
2024-02-22 15:27:39 -0500

    updated url_to_text

--- url_to_text.py
+++ url_to_text.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
 urls = pd.read_csv('URLs.csv', index_col=0)
+text = urls
 
 print(urls.to_string())
 
 for month, row in urls.iterrows():
     for stock in row:
         newCell = stock.split()
-        for link in newCell:
-            # set link equal to text version of link using web scraping
+        cellIndex = 0
+        for item in newCell:
+            newItem = # web scraping for item
+            newCell[cellIndex] = newItem
+            cellIndex += 1
+        text[month][stock] = newCell
